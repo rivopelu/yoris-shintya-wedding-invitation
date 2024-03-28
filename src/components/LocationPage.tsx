3db4ba4bdbe9565364e1542f55612d4c9b53733c
rivo pelu <yannai.ming@falkcia.com>
2024-03-29 00:18:11 +0800

    FEAT:add aos

--- src/components/LocationPage.tsx
+++ src/components/LocationPage.tsx
@@ -7,19 +7,46 @@ export interface ICoordinate {
 }
 export function LocationPage() {
   return (
-    <div className={"min-h-[300px] py-10 text-slate-800"}>
+    <div className={"min-h-[300px] py-32 text-slate-800"}>
       <Container>
         <div className={"grid lg:grid-cols-2 gap-5"}>
           <div className={"font-nunito text-center grid gap-10"}>
             <div>
-              <h1 className={"text-center lg:text-4xl text-2xl "}>
+              <h1
+                className={"text-center lg:text-4xl text-2xl "}
+                data-aos-delay="0"
+                data-aos-duration="1000"
+                data-aos="fade-up"
+              >
                 Marriage Contract
               </h1>
-              <div className={"lg:text-2xl"}>Friday, April 19th, 2021</div>
-              <div>at 14:00</div>
-              <div>Gereja GMPU Gloria Bitung, Pardo, Bitung Tengah</div>
+              <div
+                data-aos-delay="0"
+                data-aos-duration="1000"
+                data-aos="fade-up"
+                className={"lg:text-2xl"}
+              >
+                Friday, April 19th, 2021
+              </div>
+              <div
+                data-aos-delay="0"
+                data-aos-duration="1000"
+                data-aos="fade-up"
+              >
+                at 14:00
+              </div>
+              <div
+                data-aos-delay="0"
+                data-aos-duration="1000"
+                data-aos="fade-up"
+              >
+                Gereja GMPU Gloria Bitung, Pardo, Bitung Tengah
+              </div>
             </div>
             <div
+              data-aos-delay="0"
+              data-aos-duration="1000"
+              data-aos="fade-up"
               className={
                 "bg-slate-200 lg:h-[300px] h-[200px] w-full border border-slate-500"
               }
@@ -40,12 +67,41 @@ export function LocationPage() {
           ></div>
           <div className={"font-nunito text-center grid gap-10"}>
             <div>
-              <h1 className={"text-center lg:text-4xl text-2xl "}>RECEPTION</h1>
-              <div className={"lg:text-2xl"}>Friday, April 19th, 2021</div>
-              <div>at 18:00</div>
-              <div>Manembo nambo bawah</div>
+              <h1
+                data-aos-delay="300"
+                data-aos-duration="1000"
+                data-aos="fade-up"
+                className={"text-center lg:text-4xl text-2xl "}
+              >
+                RECEPTION
+              </h1>
+              <div
+                data-aos-delay="300"
+                data-aos-duration="1000"
+                data-aos="fade-up"
+                className={"lg:text-2xl"}
+              >
+                Friday, April 19th, 2021
+              </div>
+              <div
+                data-aos-delay="300"
+                data-aos-duration="1000"
+                data-aos="fade-up"
+              >
+                at 18:00
+              </div>
+              <div
+                data-aos-delay="300"
+                data-aos-duration="1000"
+                data-aos="fade-up"
+              >
+                Manembo nambo bawah
+              </div>
             </div>
             <div
+              data-aos-delay="300"
+              data-aos-duration="1000"
+              data-aos="fade-up"
               className={
                 "bg-slate-200 lg:h-[300px] h-[200px] w-full border border-slate-500"
               }
