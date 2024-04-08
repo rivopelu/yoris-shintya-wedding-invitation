4733861d9af2230815794eac156e61f7be2eccf5
rivo pelu <yannai.ming@falkcia.com>
2024-04-08 23:54:08 +0800

    FEAT:fixing

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,14 @@ import MomentPage from './components/MomentPage.tsx';
 import CommentnPage from './components/CommentnPage.tsx';
 import BottomContentPage from './components/BottomContentPage.tsx';
 import FooterComponent from './components/FooterComponent.tsx';
+import { IconButton } from '@mui/material';
+import { Pause, PlayArrow } from '@mui/icons-material';
 
 function App() {
   const audioUrl = '/assets/song.mp3';
 
   const [open, setOpen] = useState<boolean>(true);
+  const [playAudio, setPlayAudio] = useState<boolean>(false);
 
   const audio = new Audio(audioUrl);
 
@@ -27,16 +30,32 @@ function App() {
 
   function onclickOpen() {
     audio.play();
+    setPlayAudio(true);
     setOpen(false);
+  }
+
+  function onClickPlayPause() {
+    setPlayAudio((e) => {
+      if (e) {
+        audio.pause();
+        setPlayAudio(false);
+      } else {
+        audio.play();
+        setPlayAudio(true);
+      }
+    });
   }
   return (
     <>
       <section
-        className={`${open ? 'hidden' : ' '}  bg-fixed bg-contain`}
+        className={`${open ? 'hidden' : ' '}  bg-fixed bg-contain relative`}
         style={{
           backgroundImage: `url(${ASSETS.BG})`,
         }}
       >
+        <div className="fixed bg-slate-300 bottom-3 right-3 rounded-full">
+          <IconButton onClick={onClickPlayPause}>{playAudio ? <Pause /> : <PlayArrow />}</IconButton>
+        </div>
         <OnboardPopup onClickOpen={onclickOpen} open={open} />
         <>
           <Home />
