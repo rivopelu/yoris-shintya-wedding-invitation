cf90af5e1bf2eca27d2d1d28388dca27174e3e38
rivo pelu <yannai.ming@falkcia.com>
2024-04-08 22:05:54 +0800

    FEAT:fixing

--- src/components/CommentnPage.tsx
+++ src/components/CommentnPage.tsx
@@ -45,48 +45,53 @@ function CommentnPage() {
   }, []);
 
   return (
-    <Container>
-      <div className="h-1 bg-slate-400/10 my-10"></div>
-      <div className="min-h-[500px]">
-        <h1 className={'text-center mb-10 text-2xl italic'}>Wishes</h1>
-        <div className="grid gap-3">
-          <TextField
-            onChange={(e) => setValueName(e?.target?.value)}
-            value={valueName}
-            size={'small'}
-            label={'Name'}
-            placeholder={'insert your name'}
-            sx={{ background: 'rgba(255,255,255,0.2)' }}
-          />{' '}
-          <TextField
-            onChange={(e) => setValueWish(e?.target?.value)}
-            value={valueWish}
-            size={'small'}
-            label={'wishes'}
-            multiline={true}
-            rows={4}
-            placeholder={'ex : congrats shintya and yoris'}
-            sx={{ background: 'rgba(255,255,255,0.2)' }}
-          />
-          <Button disabled={valueName && valueWish ? false : true} onClick={onClick} variant={'outlined'} color={'inherit'}>
-            <div className="gap-3 flex items-center">
-              <div> Submit</div> {loading && <CircularProgress size={16} />}
+    <>
+      <div className="backdrop-blur-[2px]">
+        <div className="h-1 bg-slate-400/10 mb-10"></div>
+
+        <Container>
+          <div className="min-h-[500px]">
+            <h1 className={'text-center mb-10 text-2xl italic'}>Wishes</h1>
+            <div className="grid gap-3">
+              <TextField
+                onChange={(e) => setValueName(e?.target?.value)}
+                value={valueName}
+                size={'small'}
+                label={'Name'}
+                placeholder={'insert your name'}
+                sx={{ background: 'rgba(255,255,255,0.2)' }}
+              />
+              <TextField
+                onChange={(e) => setValueWish(e?.target?.value)}
+                value={valueWish}
+                size={'small'}
+                label={'wishes'}
+                multiline={true}
+                rows={4}
+                placeholder={'ex : congrats shintya and yoris'}
+                sx={{ background: 'rgba(255,255,255,0.2)' }}
+              />
+              <Button disabled={valueName && valueWish ? false : true} onClick={onClick} variant={'outlined'} color={'inherit'}>
+                <div className="gap-3 flex items-center">
+                  <div> Submit</div> {loading && <CircularProgress size={16} />}
+                </div>
+              </Button>
             </div>
-          </Button>
-        </div>
-        <div className={'mt-10'}>
-          {data.map((item, i) => (
-            <div key={i} className={'border-b pb-2 '}>
-              <div className={'flex items-center gap-2'}>
-                <Avatar sx={{ height: 30, width: 30 }} />
-                <div>{item?.name}</div>
-              </div>
-              <div>{item?.wish}</div>
+            <div className={'mt-10 grid gap-4 max-h-[300px] overflow-y-auto'}>
+              {data.map((item, i) => (
+                <div key={i} className={'border border-slate-400  p-4 rounded-md '}>
+                  <div className={'flex items-center gap-2'}>
+                    <Avatar sx={{ height: 30, width: 30 }} />
+                    <div>{item?.name}</div>
+                  </div>
+                  <div className="mt-3  pl-10">{item?.wish}</div>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </div>
+        </Container>
       </div>
-    </Container>
+    </>
   );
 }
 
--- src/components/OnboardPopup.tsx
+++ src/components/OnboardPopup.tsx
@@ -1,15 +1,26 @@
-import { ASSETS } from "../assets/assets.ts";
-import { DATA_CONSTANTS } from "../constants/data.ts";
-import { Box, Fade, Modal } from "@mui/material";
+import { useLocation } from 'react-router-dom';
+import { ASSETS } from '../assets/assets.ts';
+import { DATA_CONSTANTS } from '../constants/data.ts';
+import { Box, Fade, Modal } from '@mui/material';
+import { useEffect } from 'react';
 
 export function OnboardPopup(props: IProps) {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(useLocation().search);
+  const to = searchParams.get('to');
   const style = {
-    position: "absolute" as const,
+    position: 'absolute' as const,
     top: 0,
     left: 0,
-    width: "100vw",
-    bgcolor: "black",
+    width: '100vw',
+    bgcolor: 'black',
   };
+
+  useEffect(() => {
+    if (to) {
+      console.log(to);
+    }
+  }, [to]);
 
   function component() {
     return (
@@ -19,43 +30,18 @@ export function OnboardPopup(props: IProps) {
           backgroundImage: `url(${ASSETS.COVER})`,
         }}
       >
-        <div
-          className={
-            "h-full w-full flex items-center justify-evenly text-slate-300 flex-col py-[7vh] lg:bg-black/60 bg-black/80 "
-          }
-        >
-          <div className={"flex flex-col text-center items-center gap-3 "}>
-            <img
-              data-aos="fade-up"
-              data-aos-duration="1000"
-              className={"h-[100px] sm:h-[200px] w-fit "}
-              src={ASSETS.LV_1}
-              alt={"love"}
-            />
+        <div className={'h-full w-full flex items-center justify-evenly text-slate-300 flex-col py-[7vh] lg:bg-black/60 bg-black/80 '}>
+          <div className={'flex flex-col text-center items-center gap-3 '}>
+            <img data-aos="fade-up" data-aos-duration="1000" className={'h-[100px] sm:h-[200px] w-fit '} src={ASSETS.LV_1} alt={'love'} />
 
-            <div
-              data-aos-delay="50"
-              data-aos-duration="1000"
-              data-aos="fade-up"
-            >
+            <div data-aos-delay="50" data-aos-duration="1000" data-aos="fade-up">
               {"Hello, You're Invited"}
             </div>
-            <div
-              data-aos-delay="120"
-              data-aos-duration="1000"
-              data-aos="fade-up"
-              className={"text-2xl font-bold "}
-            >
+            <div data-aos-delay="120" data-aos-duration="1000" data-aos="fade-up" className={'text-2xl font-bold '}>
               The Wedding Of
             </div>
-            <div
-              data-aos-delay="200"
-              data-aos-duration="1000"
-              data-aos="fade-up"
-              className={"tangerine-bold  text-6xl lg:text-[120px]"}
-            >
-              {DATA_CONSTANTS.GROOM_NICK_NAME} &{" "}
-              {DATA_CONSTANTS.BRIDE_NICK_NAME}
+            <div data-aos-delay="200" data-aos-duration="1000" data-aos="fade-up" className={'tangerine-bold  text-6xl lg:text-[120px]'}>
+              {DATA_CONSTANTS.GROOM_NICK_NAME} & {DATA_CONSTANTS.BRIDE_NICK_NAME}
             </div>
           </div>
 
@@ -63,20 +49,16 @@ export function OnboardPopup(props: IProps) {
             data-aos-delay="240"
             data-aos-duration="1000"
             data-aos="fade-up"
-            className={
-              "flex flex-col text-center border-b w-full  text-xl p-5 rounded-md border-slate-700/50"
-            }
+            className={'flex flex-col text-center border-b w-full  text-xl p-5 rounded-md border-slate-700/50'}
           >
-            <div>{"To : Tessalonika Laluraga"}</div>
+            {to && <div>{'To : ' + to}</div>}
           </div>
           <div>
             <button
               data-aos-delay="240"
               data-aos-duration="1000"
               onClick={props.onClickOpen}
-              className={
-                "border mt-4 px-5 py-2 border-white/20 bg-white/5 hover:bg-white/10 active:bg-white/20 duration-200"
-              }
+              className={'border mt-4 px-5 py-2 border-white/20 bg-white/5 hover:bg-white/10 active:bg-white/20 duration-200'}
             >
               OPEN INVITATION
             </button>
