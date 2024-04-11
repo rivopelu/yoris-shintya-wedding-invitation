ef819a597f92a29974ce3824e3121df53f6eb55d
rivo pelu <yannai.ming@falkcia.com>
2024-04-11 16:42:30 +0800

    FEAT:fixing

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -6,7 +6,7 @@ function FooterComponent() {
       <Container>
         <div className="grid gap-2  font-nunito text-center">
           <a href="https://www.rivopelu.com" className="border-b">
-            © 2024 Rivo Pelu - All Rights Reserved
+            © 2024 - All Rights Reserved
           </a>
           <h1>For Yoris And Shintya</h1>
         </div>
