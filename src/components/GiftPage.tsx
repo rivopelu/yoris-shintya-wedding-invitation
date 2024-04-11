bb00360894ba172890e7d52e1ab4a56639563a9b
rivo pelu <yannai.ming@falkcia.com>
2024-04-11 19:47:30 +0800

    FEAT:fixing

--- src/components/GiftPage.tsx
+++ src/components/GiftPage.tsx
@@ -4,6 +4,16 @@ import { ASSETS } from '../assets/assets';
 
 export function GiftPage() {
   const bankNumber = '7707701276';
+  const handleCopyText = () => {
+    navigator.clipboard
+      .writeText(bankNumber)
+      .then(() => {
+        alert('Text copied to clipboard');
+      })
+      .catch((error) => {
+        console.error('Failed to copy text: ', error);
+      });
+  };
   return (
     <Container className="py-10">
       <div className="text-center font-nunito grid gap-3">
@@ -16,7 +26,7 @@ export function GiftPage() {
         <div data-aos-duration="1000" data-aos="fade-up" className="flex items-center text-2xl justify-center gap-3">
           <img src={ASSETS.BNI} className="h-[16px]" />
           <div>{bankNumber}</div>
-          <IconButton>
+          <IconButton onClick={handleCopyText}>
             <CopyAllOutlined />
           </IconButton>
         </div>
